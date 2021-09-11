b0a05f0113430f87197551f26e28357e08558834
Muyaz khan <muyazkhan06@gmail.com>
2021-09-11 15:19:36 +0600

    h

---
+++ java/app.js
@@ -0,0 +1,42 @@
+// let counter = document.querySelector('.counter');
+
+// let count = 1;
+
+
+// setInterval(() => {
+//   if(count < 75);
+//   count++;
+//   counter.innerText = count;
+  
+// }, 100);
+
+
+
+let counters = document.querySelectorAll('.counter')
+let time = 300;
+
+counters.forEach(counter =>{
+  let updateCount = () => {
+  let target = +counter.getAttribute('data-target');
+  let count = +counter.innerText;
+
+  let incriment = target / time;
+
+  if(count < target){
+    counter.innerHTML = Math.ceil(count + incriment);
+    setTimeout( updateCount, 20);
+
+  } else {
+    counter.innerText = target;
+  }
+  }; 
+  updateCount()
+});
+
+let healthCondition = document.querySelector('.health');
+
+setTimeout(()  =>{
+   healthCondition.innerText = "You are Fit!"
+  
+
+},5980);
